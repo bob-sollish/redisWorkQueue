1958e71d20ef04542703262b4c35bf64436a2bd8
Bob Sollish <bob.sollish@zozi.com>
2016-07-07 09:58:18 -0700

    initial commit

---
+++ src/main/java/RedisWorkQueue/redisWorkQueue/WorkConsumer.java
@@ -0,0 +1,7 @@
+package RedisWorkQueue.redisWorkQueue;
+
+public class WorkConsumer {
+   public static void main(String[] args) {
+      System.out.println("I am the WorkConsumer!");
+   }
+}
---
+++ src/main/java/RedisWorkQueue/redisWorkQueue/WorkProducer.java
@@ -0,0 +1,7 @@
+package RedisWorkQueue.redisWorkQueue;
+
+public class WorkProducer {
+   public static void main(String[] args) {
+      System.out.println("I am the WorkProducer!");
+   }
+}
